f8bb9bd95274b14cd1b48ffd3b40aa3284f72a78
Justin Elszasz <jtelszasz@gmail.com>
2021-02-23 19:24:31 -0500

    standing up shiny app

--- R/functions.R
+++ R/functions.R
@@ -1,15 +1,40 @@
 library(tidyverse)
 library(lubridate)
+library(readxl)
 
+load_raw_ledger_flat_file <- function(){
+  # load raw ledger export from city dynamics gp
+  # temporary while data pipeline built out
+  
+  start_time <- Sys.time()
+  message(paste(start_time, "Starting data load"))
 
-load_raw_gp_data <- function(){
+  col_types <- c(
+    rep("text", 3),
+    "date",
+    "date",
+    rep("text", 4),
+    "numeric",
+    "numeric",
+    rep("text", 6),
+    "numeric",
+    "numeric",
+    "numeric",
+    "text",
+    "text",
+    "numeric"
+  )
   
-  df <- read.table(
-    "data/raw/OCFY2020.txt", 
-    sep = "\t", 
-    nrows = 1000, 
-    fill = T)
+  df <- read_excel(
+    "data/raw/OpenCheck FY2020.xlsx",
+    col_types = col_types
+  )
   
+  end_time <- Sys.time()
+  message(paste(end_time, "Data load complete"))
+  message(paste("Time to complete: ", end_time - start_time))
+  
+  df
 }
 
 
---
+++ R/server.R
@@ -0,0 +1,9 @@
+source("functions.R")
+
+ledger_raw <- load_raw_ledger_flat_file()
+
+shinyServer({
+  
+  
+  
+})
